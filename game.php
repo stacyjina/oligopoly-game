8978d3063bb791c96dc9669ab04cfdd4da1494aa
Anastasia <aasotnikova_1@edu.hse.ru>
2024-03-25 22:11:41 +0300

    add class game and sliders

--- game.php
+++ game.php
@@ -1,28 +1,87 @@
 <?php
     include("db.php");
 
-    function save_choice($conn, $gamename, $username, $round, $y, $r) {
-        $query = "insert into moves (game, login, round, yield, pr) 
-                values ('{$gamename}', '{$username}', {$round}, {$y}, {$r})";
-        mysqli_query($conn, $query);
+    class Game {
+
+        private $conn;
+        private $cur_round;
+        private $login;
+        private $gamename;
+        public $num_players = 5;
+        public $max_price = 50;
+        private $costs = 20;
+
+        function load_game($conn, $login, $gamename, $round) {
+            $this->conn = $conn;
+            $this->login = $login;
+            $this->gamename = $gamename;
+            $this->cur_round = $round;
+        }
+
+        function save_choice($y, $r) {
+            $query = "insert into moves (game, login, round, yield, pr) 
+                    values ('{$this->gamename}', '{$this->login}', {$this->cur_round}, {$y}, {$r})";
+            mysqli_query($this->conn, $query);
+        }
+
+        function get_round_results() {
+            $query = "select login, yield, pr from moves where game = '{$this->gamename}' and round = {$this->cur_round}";
+            $res = mysqli_query($this->conn, $query)->fetch_all(MYSQLI_ASSOC);
+            $list = [];
+            $y = 0;
+            foreach ($res as $row) {
+                $list[substr($row["login"], -1, 1)] = ["yield" => $row["yield"], "pr" => $row["pr"]];
+                $y += $row["yield"];
+            }
+            $p = $this->max_price * $this->num_players - $y;
+            echo "y = {$y}, p = {$p} <br>";
+            $res = [];
+            foreach ($list as $key => $value) {
+                // echo $key;
+                // echo "<br>";
+                $res[$key] = ["y" => $value["yield"], 
+                                "r" => $value["pr"], 
+                                "p" => $p * (1 + 0.002 * $value["pr"]),
+                                "profit" => $p * (1 + 0.002 * $value["pr"]) * $value["yield"] - 
+                                                $this->costs * $value["yield"] - $value["pr"]
+                            ];
+                $query = "insert into rounds (game, login, round, profit) 
+                            values ('{$this->gamename}', '{$this->login}', {$this->cur_round}, {$res[$key]["profit"]})";
+                // mysqli_query($this->conn, $query);
+                // foreach ($res[$key] as $k => $val) {
+                //     echo "{$k} => {$val} <br>";
+                // }
+            }
+            return $res;
+        }
+
+        function get_final_results() {
+
+        }
     }
 
-    function get_round_results($conn, $gamename, $round) {
-        $query = "select login, yield, pr from moves where game = '{$gamename}' and round = {$round}";
-        $res = mysqli_query($conn, $query)->fetch_all(MYSQLI_ASSOC);
-        $list = [];
-        $y = 0;
-        foreach ($res as $row) {
-            $list[substr($row["login"], -1, 1)] = ["yield" => $row["yield"], "pr" => $row["pr"]];
-            $y += $row["yield"];
-        }
-        $p = 100 - $y;
-        echo "y = {$y}, p = {$p} <br>";
-        foreach ($list as $key => $value) {
-            echo $key;
-            echo "<br>";
-            foreach ($value as $k => $val) {
-                echo "{$k} => {$val} <br>";
-            }
-        }
-    }
+    // function save_choice($conn, $gamename, $username, $round, $y, $r) {
+    //     $query = "insert into moves (game, login, round, yield, pr) 
+    //             values ('{$gamename}', '{$username}', {$round}, {$y}, {$r})";
+    //     mysqli_query($conn, $query);
+    // }
+
+    // function get_round_results($conn, $gamename, $round) {
+    //     $query = "select login, yield, pr from moves where game = '{$gamename}' and round = {$round}";
+    //     $res = mysqli_query($conn, $query)->fetch_all(MYSQLI_ASSOC);
+    //     $list = [];
+    //     $y = 0;
+    //     foreach ($res as $row) {
+    //         $list[substr($row["login"], -1, 1)] = ["yield" => $row["yield"], "pr" => $row["pr"]];
+    //         $y += $row["yield"];
+    //     }
+    //     $p = 100 - $y;
+    //     echo "y = {$y}, p = {$p} <br>";
+    //     foreach ($list as $key => $value) {
+    //         echo $key;
+    //         echo "<br>";
+    //         foreach ($value as $k => $val) {
+    //             echo "{$k} => {$val} <br>";
+    //         }
+    //     }
+    // }
--- game_page.php
+++ game_page.php
@@ -5,6 +5,8 @@
     $login = $_SESSION["login"];
     $gamename = $_SESSION["gamename"];
     $round = $_SESSION["round"];
+    $game = new Game();
+    $game->load_game($conn, $login, $gamename, $round);
 ?>
 
 <!DOCTYPE html>
@@ -22,21 +24,46 @@
     ?>
 
     <?php
-        if (isset($_POST["start"])) {
+        if (isset($_POST["start"]) or isset($_POST["move"])) {
             echo "There will be game <br>";
             echo "Demand function of an aggregated consumer:";
-            echo "$$ p = 100 - \\Sigma_{i = 1}^n y_i $$";
+            $price = $game->max_price * $game->num_players;
+            echo "$$ p = {$price} - \\Sigma_{i = 1}^n y_i $$";
             echo "Your firm's profit function:";
-            echo "$$ \\pi = (p + 0,2 r \\cdot p) \\cdot y - 10y - 100r $$";
-            get_round_results($conn, $gamename, 1);
+            echo "$$ \\pi = (p + 0,2 r \\cdot p) \\cdot y - 20y - 100r $$";
+            // $game->save_choice(20, 60);
             if ($round != 1) {
                 $table_html = "<table>";
+                $table_html .= "<colgroup> 
+                                    <col span='4'> 
+                                    <col style='border: 2px solid black'> 
+                                </colgroup>";
                 $table_html .= "<tr>";
                 $table_html .= "<th> Player </th> <th> Yield (Y) </th> <th> PR (R) </th> <th> Price (P) </th> <th> Profit </th>";
                 $table_html .= "</tr>";
+                $res = $game->get_round_results();
+                foreach ($res as $id => $list) {
+                    $table_html .= "<tr> <td> Player {$id} </td>";
+                    $table_html .= "<td> {$list["y"]} </td> <td> {$list["r"]} </td>";
+                    $table_html .= "<td> {$list["p"]} </td> <td> {$list["profit"]} </td> </tr>";
+                }
+                $table_html .= "</table>";
                 // здесь потом будет цикл наверн
                 echo $table_html;
             }
+            if (isset($_POST["move"])) {
+                echo "Your choice: Y = {$_POST["yield"]}, PR = {$_POST["pr"]}";
+            } else {
+            $sliders = "<form class='sliders' method='post' action='game_page.php'>";
+            $sliders .= "<div class='sliders'> 
+                            <input type='range' min='0' max='{$game->max_price}' value='0' name='yield'>
+                            <input type='range' min='0' max='{$game->max_price}' value='0' name='pr'>
+                        </div>";
+            $sliders .= "<button type='submit' name='move'> Save choice </button>";
+            $sliders .= "</form>";
+            echo $sliders;
+            }
+
         } else {
             echo "Game rules: ................... <br> Press a button below if you are ready to start.<br>";
             echo '<form class="start" method="POST" action="game_page.php"> 
